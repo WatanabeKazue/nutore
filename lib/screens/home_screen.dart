77afff73e20bf91e61d809ea207021be673ea8cf
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-07-22 13:40:20 +0900

    エラーでました

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,9 +6,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<DropdownMenuItem> _menuItems = [];
-  int _numberOfQuestions = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,25 +16,15 @@ class _HomeScreenState extends State<HomeScreen> {
               Image.asset("assets/images/image_title.png"),
               Text("問題数を選択して「スタート」ボタンを押してください"),
               //TODO プルダウン選択肢
-              const SizedBox(
-                height: 75.0,
+              Expanded(
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    padding: EdgeInsets.only(bottom: 16.0),
+                    child: RaisedButton(
+                      child: Text("スタート"),
+                      onPressed: nulle,
               ),
-              DropdownButton(
-                items: _menuItems,
-                value: _numberOfQuestions,
-                //null safety (T?なので型指定必要)
-                //final ValueVChanged<T?>? onChanged
-                //https: //api.flutter.dev/flutter/material/DropdownButton-class.htm
-                icon: Icon(Icons.skip_next),
-                //label: Text("スタート"),
-                //onPressed: () => print("おしたで～"),
-                // style: ElevatedButton.styleFrom(
-                //     primary: Icon(Icons.)
-              ),
-              RaisedButton(
-                child: Text("スタート"),
-                onPressed: null,
-              ),
+                  ))
             ],
           ),
         ),
