77a7587e6e5138838f8280bc020e8da8f5aac083
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-07-24 13:46:29 +0900

    エラーでました

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
+    print("ヨコ幅の論理ピクセル:$screenWidth");
+    print("タテ幅の論理ピクセル:$screenHeight");
+  }
 
   List<DropdownMenuItem<int>> _menuItems = [];
   int _numberOfQuestions = 0;
@@ -20,9 +27,9 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void setMenuItems() {
-    _menuItems.add(DropdownMenuItem(value: 10, child: Text("10"),));
-    _menuItems.add(DropdownMenuItem(value: 20, child: Text("20"),));
-    _menuItems.add(DropdownMenuItem(value: 30, child: Text("30"),));
+    //_menuItems.add(DropdownMenuItem(value: 10, child: Text("10"),));
+   // _menuItems.add(DropdownMenuItem(value: 20, child: Text("20"),));
+   // _menuItems.add(DropdownMenuItem(value: 30, child: Text("30"),));
 
     _menuItems = add(DropdownMenuItem(value: 10, child: Text(10.toString()),))
        ..add(DropdownMenuItem(value: 10, child: Text(10.toString()),))
@@ -40,42 +47,74 @@ class _HomeScreenState extends State<HomeScreen> {
               Text("問題数を選択して「スタート」ボタンを押してください"),
               //TODO プルダウン選択肢
               SizedBox(height: 50.0,),
-              DropdownButton(items: _menuItems,
-               value: _numberOfQuestions ,
-              onChanged: (value) => print(value.toString()),
-              setState(() {
-                _numberOfQuestions = selectedValue;
-              });
-              } ,
-              ),
-              [
-                DropdownMenuItem(child: Container()),
-                DropdownMenuItem(child: Container()),
-                DropdownMenuItem(child: Container()),
+              DropdownButton(items: [
+              DropdownMenuItem(child: child),
+                DropdownMenuItem(child: child),
+                DropdownMenuItem(child: child),
+
               ],),
-              Expanded(
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    padding: EdgeInsets.only(bottom: 16.0),
-                    child: ElevatedButton.icon(
-                        icon: Icon(Icons.ac_unit), // TODO あとで書き換え
-                        label: Text("スタート"),
-                        onPressed: () => print("おしたで～"),
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.purpleAccent,
-                            onPrimary: Colors.black,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(
-                                  20.0),),
-                            )
-                        )
-                    ),
-                  ))
-            ],
+               Expanded(
+                 child: Container(
+                   alignment: Alignment.bottomCenter,
+                  // padding: EdgeInsets.only(bottom: 12.0),
+                   child: RaisedButton.icon(
+                     icon: Icon(Icons.skip_next),
+                     label: Text("スタート"),
+                     onPressed: () => print("おしたで～"),
+                     style: ElevatedButton.styleFrom(
+                       primary:  Colors.brown,
+                       onPrimary: Colors.yellowAccent,
+                       shape: RoundedRectangleBorder()
+                         borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                     )
+                   ),
+                 ),
+               )
+  ],
           ),
         ),
       ),
     );
+             // DropdownB)utton(items: _menuItems,
+             //  value: _numberOfQuestions ,
+             // onChanged: (value) => print(value.toString()),
+
+
+
+
+              //setState(() {
+               // _numberOfQuestions = selectedValue;
+             // });
+             // } ,
+             // ),
+             // [
+               // DropdownMenuItem(child: Container()),
+               // DropdownMenuItem(child: Container()),
+               // DropdownMenuItem(child: Container()),
+             // ],),
+              //Expanded(
+                 // child: Container(
+                    //alignment: Alignment.bottomCenter,
+                  //  padding: EdgeInsets.only(bottom: 16.0),
+                    //child: ElevatedButton.icon(
+                     //   icon: Icon(Icons.ac_unit), // TODO あとで書き換え
+                       // label: Text("スタート"),
+                     //   onPressed: () => print("おしたで～"),
+                     //   style: ElevatedButton.styleFrom(
+                         //   primary: Colors.purpleAccent,
+                       //     onPrimary: Colors.black,
+                          //  shape: RoundedRectangleBorder(
+                            //  borderRadius: BorderRadius.all(Radius.circular(
+                            //      20.0),),
+                        //    )
+                     //   )
+                 //   ),
+                //  ))
+          //  ],
+        //  ),
+      //  ),
+    //  ),
+  //  );
   }
 //TODO 140
 
